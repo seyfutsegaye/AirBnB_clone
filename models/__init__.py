1b8a299ea580409a3f9b4cb311f91a143ad25975
seyfu tsegaye <99539647+seyfutsegaye@users.noreply.github.com>
2023-02-12 14:15:53 +0300

    Task

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""__init__ module"""
-from models.engine.file_storage import FileStorage
 
+from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
 storage.reload()
